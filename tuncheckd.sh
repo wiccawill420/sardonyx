44915c53f9f3c65e7dc0ff843e61bc6ec0cc67c6
William <wiccawill420@gmail.com>
2017-01-07 15:53:28 -0800

    active tunnel check

---
+++ tuncheckd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# tuncheckd.sh
+# By: William Lynn
+# Continues to check for the VPN tunnel while Kodi is running
+# Kills Kodi if the tunnel ever goes down
+
+while true; do
+	sleep 5m
+	if [ $(ifconfig|grep tun > /dev/null;echo $?) -ne 0 ]
+		then
+			echo "Tunnel Died! Closed Kodi as a precaution." | wall
+			sudo killall -9 -g kodi
+			exit 1
+	fi
+done
