b0e63c8417959f4317071d533f4c29ab764540e2
brucethehoon <amorrow@gmail.com>
2017-01-08 14:24:45 -0800

    set up opal

---
+++ opal_start.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#By: William
+
+function fMAIN(){
+echo -e "${white}#############################${white}#${plain}"
+echo -e "${white}#                            ${white}#${plain}"
+echo -e "${white}#      ${lightcyan}Welcome to ${lightpurple}Opal ${red}<3    ${white}#${plain}"
+echo -e "${white}#                            ${white}#${plain}"
+echo -e "${white}# ${green}To watch shows, type ${blue}shows ${white}#${plain}"
+echo -e "${white}# ${green}To play games, type ${blue}games  ${white}#${plain}"
+echo -e "${white}# ${green}To setup roms, type ${blue}roms   ${white}#${plain}"
+echo -e "${white}#                            ${white}#${plain}"
+echo -e "${white}# ${green}Have fun!                  ${white}#${plain}"
+echo -e "${white}#                            ${white}#${plain}"
+echo -e "${white}#############################${white}#${plain}"
+echo ""
+}
+
+function fCOLOR(){		#Define colors
+green='\e[0;32m'
+plain='\e[0m'
+cyan='\e[0;36m'
+red='\e[0;31m'
+purple='\e[0;35m'
+brown='\e[0;33m'
+lightblue='\e[1;30m'
+lightgreen='\e[1;32m'
+lightcyan='\e[1;36m'
+lightred='\e[1;31m'
+lightpurple='\e[1;35m'
+yellow='\e[1;33m'
+blue='\e[1;34m'
+white='\e[1;37m'
+}
+
+fCOLOR
+fMAIN
